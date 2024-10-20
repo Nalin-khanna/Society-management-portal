54da963ae9bb61cec3cfb276c21b67d3beca33d4
Nalin-khanna <nalinkhanna17@gmail.com>
2024-10-20 20:38:12 +0530

    first commit

--- server/db/db.js
+++ server/db/db.js
@@ -4,6 +4,7 @@ const connectToDatabase = async () => {
     try {
         const connection = await mongoose.connect(process.env.MONGO_URL);
         console.log("Connected to database: ${connection.connection.host}");
+        console.log("hello")
     } catch (error) {
         console.error(`Error connecting to database: ${error.message}`);
         process.exit(1);
